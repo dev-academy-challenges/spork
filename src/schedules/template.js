da6ec8ad0059f403507dab9cb3e46253d3474845
Kelly Keating <keating.kel@gmail.com>
2022-08-02 12:07:50 +1200

    remove solutions

--- src/schedules/template.js
+++ src/schedules/template.js
@@ -29,35 +29,13 @@ const template = (w) =>
       w.deploy('kata-data-structures', 'bowling-kata')
     ),
     w.on(w.week(1), w.thu(), w.all(), w.deploy('tdd-bowling-kata', 'conways')),
-    w.on(
-      w.week(1),
-      w.thu(),
-      w.welly(),
-      w.deploy('tdd-bowling-kata-solution', 'conways-solution')
-    ),
     w.on(w.week(1), w.fri(), w.all(), w.deploy('ascii-art-reader')),
-    w.on(w.week(1), w.fri(), w.welly(), w.deploy('ascii-art-reader-solution')),
     w.on(w.week(2), w.mon(), w.all(), w.deploy('express-server')),
-    w.on(w.week(2), w.mon(), w.welly(), w.deploy('express-server-solution')),
     w.on(w.week(2), w.tue(), w.all(), w.deploy('server-side-rendering')),
-    w.on(
-      w.week(2),
-      w.tue(),
-      w.welly(),
-      w.deploy('server-side-rendering-solution')
-    ),
     w.on(w.week(2), w.wed(), w.all(), w.deploy('pupparazzi')),
-    w.on(w.week(2), w.wed(), w.welly(), w.deploy('pupparazzi-solution')),
     w.on(w.week(2), w.thu(), w.all(), w.deploy('heroku-checklist')),
     w.on(w.week(3), w.mon(), w.all(), w.deploy('knex-todo-cli')),
-    w.on(w.week(3), w.mon(), w.welly(), w.deploy('knex-todo-cli-solution')),
     w.on(w.week(3), w.tue(), w.all(), w.deploy('knex-joins-stories')),
-    w.on(
-      w.week(3),
-      w.tue(),
-      w.welly(),
-      w.deploy('knex-joins-stories-solution')
-    ),
     w.on(w.week(3), w.wed(), w.all(), w.deploy('database-diagram')),
     w.on(w.week(3), w.wed(), w.welly(), w.deploy('knex-relationships-stories')),
     w.on(w.week(3), w.wed(), w.except(w.welly()), w.deploy('dreamfest')),
@@ -75,35 +53,16 @@ const template = (w) =>
     ),
     w.on(
       w.week(4),
-      w.mon(),
-      w.welly(),
-      w.deploy('react-paws-for-effect-solution', 'charlottes-web-log-solution')
-    ),
-    w.on(
-      w.week(4),
       w.tue(),
       w.all(),
       w.deploy('broken-kaleidoscope', 'memory', 'enspiraled')
     ),
-    w.on(
-      w.week(4),
-      w.tue(),
-      w.welly(),
-      w.deploy(
-        'broken-kaleidoscope-solution',
-        'memory-solution',
-        'enspiraled-solution'
-      )
-    ),
-
     w.on(w.week(4), w.wed(), w.all(), w.deploy('worldwide-routing')),
-    w.on(w.week(4), w.wed(), w.welly(), w.deploy('worldwide-routing-solution')),
     w.on(w.week(4), w.thu(), w.all(), w.deploy('boilerplate-react-webpack')),
     w.on(w.week(5), w.mon(), w.all(), w.deploy('charlottes-web-log-api')),
     w.on(w.week(5), w.tue(), w.all(), w.deploy('react-to-web-api')),
     w.on(w.week(5), w.wed(), w.all(), w.deploy('consuming-external-apis')),
     w.on(w.week(6), w.mon(), w.except(w.online()), w.deploy('redux-minimal')),
-    w.on(w.week(6), w.mon(), w.welly(), w.deploy('redux-minimal-solution')),
     w.on(
       w.week(6),
       w.mon(),
@@ -118,7 +77,6 @@ const template = (w) =>
       )
     ),
     w.on(w.week(6), w.tue(), w.except(w.online()), w.deploy('sweet-as-beers')),
-    w.on(w.week(6), w.tue(), w.welly(), w.deploy('sweet-as-beers-solution')),
     w.on(w.week(6), w.tue(), w.online(), w.deploy('async-redux-stories')),
     w.on(
       w.week(6),
