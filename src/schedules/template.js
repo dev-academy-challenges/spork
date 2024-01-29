7f567dbc594b415f590539fca3cd15d1cd46a370
Gerard Paapu <gerard@gerardpaapu.com>
2024-01-30 10:28:16 +1300

    feat: add whats on tvite to templates

--- src/schedules/template.js
+++ src/schedules/template.js
@@ -30,11 +30,31 @@ const template = (w) =>
       w.deploy('kata-data-structures', 'bowling-kata')
     ),
     w.on(w.week(1), w.thu(), w.all(), w.deploy('tdd-bowling-kata', 'conways')),
-    w.on(w.week(1), w.fri(), w.all(), w.deploy('kata-typescript')),
-    w.on(w.week(2), w.mon(), w.all(), w.deploy('react-paws-for-effect', 'charlottes-web-log')),
-    w.on(w.week(2), w.tue(), w.all(), w.deploy('pixel-party', 'broken-kaleidoscope', 'enspiraled')),
+    w.on(
+      w.week(1),
+      w.fri(),
+      w.all(),
+      w.deploy('kata-typescript', 'whats-on-tvite')
+    ),
+    w.on(
+      w.week(2),
+      w.mon(),
+      w.all(),
+      w.deploy('react-paws-for-effect', 'charlottes-web-log')
+    ),
+    w.on(
+      w.week(2),
+      w.tue(),
+      w.all(),
+      w.deploy('pixel-party', 'broken-kaleidoscope', 'enspiraled')
+    ),
     w.on(w.week(2), w.wed(), w.all(), w.deploy('worldwide-routing')),
-    w.on(w.week(2), w.thu(), w.all(), w.deploy('react-form-demo', 'kata-react-forms', 'boilerplate-react')),
+    w.on(
+      w.week(2),
+      w.thu(),
+      w.all(),
+      w.deploy('react-form-demo', 'kata-react-forms', 'boilerplate-react')
+    ),
     w.on(w.week(2), w.fri(), w.except(w.akl()), w.deploy('lightning-talks')),
     w.on(w.week(3), w.mon(), w.all(), w.deploy('consuming-external-apis')),
     w.on(w.week(3), w.tue(), w.all(), w.deploy('query-em-all')),
@@ -51,12 +71,7 @@ const template = (w) =>
       w.all(),
       w.deploy('express-server', 'art-gallery')
     ),
-    w.on(
-      w.week(4),
-      w.tue(),
-      w.all(),
-      w.deploy('pupparazzi')
-    ),
+    w.on(w.week(4), w.tue(), w.all(), w.deploy('pupparazzi')),
     w.on(w.week(4), w.wed(), w.all(), w.deploy('consuming-external-apis')),
     w.on(w.week(4), w.thu(), w.all(), w.deploy('boilerplate-react-api')),
     w.on(w.week(5), w.mon(), w.all(), w.deploy('knex-todo-cli')),
